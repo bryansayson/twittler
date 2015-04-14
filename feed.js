f62573fe57e80beeee5f07d8f00f794c4733f3a5
bryan <bry_angelo@yahoo.com>
2015-04-13 22:49:53 -0700

    twittler

--- feed.js
+++ feed.js
@@ -1,25 +1,52 @@
- $(document).ready(
+$(document).ready(
 
     function updateTweets () {
         var $body = $('.tweetbox');
         var index = streams.home.length - 1;
         while(index >= 0){
           var tweet = streams.home[index];
-          var $tweet = $('<div></div>');
-          $tweet.text('@' + tweet.user + ': ' + tweet.message + tweet.created_at);
+          var $tweet = $('<div class = "tweet"> </div>');
+          var ago = moment().startOf(tweet.created_at).fromNow();
+          $tweet.html('@<span class="user_class">' + tweet.user + '</span>: ' + tweet.message + " tweeted " + ago);
           $tweet.appendTo($body);
           index -= 1;
           }
     }
 );
 
-$('.newtweets').on('click', function() {
+$(document).ready(function () {
+
+  $('.newtweets').on('click', function() {
+      var $body = $('.tweetbox');
+      $body.html('');
       var index = streams.home.length -1
       while(index >= 0){
           var tweet = streams.home[index];
-          var $tweet = $('<div></div>');
-          $tweet.text('@' + tweet.user + ': ' + tweet.message + tweet.created_at);
+          var $tweet = $('<div class = "tweet"> </div>');
+          var ago = moment().startOf(tweet.created_at).fromNow();
+          $tweet.html('@<span class="user_class">' + tweet.user + '</span>: ' + tweet.message + " tweeted " + ago);
           $tweet.appendTo($body);
           index -= 1;
           }
     });
+});
+
+$(document).ready(function () {
+
+  $('.user_class').on('click', function() {
+      var username = $(this).text();
+      var $body = $('.tweetbox');
+      $body.html('');
+      var index = streams.home.length -1
+      while(index >= 0) {
+      var tweet = streams.home[index];
+          if (username == tweet.user) {
+          var $tweet = $('<div class = "tweet"> </div>');
+          var ago = moment().startOf(tweet.created_at).fromNow();
+          $tweet.html('@<span class="user_class">' + tweet.user + '</span>: ' + tweet.message + " tweeted " + ago);
+          $tweet.appendTo($body);
+          index -= 1;
+          }
+      }
+    });
+});
